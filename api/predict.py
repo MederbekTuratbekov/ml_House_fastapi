9cd44cb0cb180320de34abc3d0adc34e24ac0633
Neo <mrrneo@outlook.com>
2025-08-19 17:51:14 +0600

    ...

--- api/predict.py
+++ api/predict.py
@@ -9,36 +9,17 @@ model = joblib.load('model.pkl')
 
 predict_router = APIRouter(prefix='/predict', tags=['Predict Price'])
 
+nei = ['Blueste', 'BrDale', 'BrkSide', 'ClearCr', 'CollgCr', 'Crawfor', 'Edwards', 'Gilbert', 'IDOTRR', 'MeadowV',
+       'Mitchel', 'NAmes', 'NPkVill', 'NWAmes', 'NoRidge', 'NridgHt', 'OldTown', 'SWISU', 'Sawyer', 'SawyerW',
+       'Somerst', 'StoneBr', 'Timber', 'Veenker']
+
 @predict_router.post('/')
 async def predict_price(house: HousePredictSchema):
     house_dict = dict(house)
 
     new_neighborhood = house_dict.pop('Neighborhood')
     neighborhood1_0 = [
-        1 if new_neighborhood == 'Blueste' else 0,
-        1 if new_neighborhood == 'BrDale' else 0,
-        1 if new_neighborhood == 'BrkSide' else 0,
-        1 if new_neighborhood == 'ClearCr' else 0,
-        1 if new_neighborhood == 'CollgCr' else 0,
-        1 if new_neighborhood == 'Crawfor' else 0,
-        1 if new_neighborhood == 'Edwards' else 0,
-        1 if new_neighborhood == 'Gilbert' else 0,
-        1 if new_neighborhood == 'IDOTRR' else 0,
-        1 if new_neighborhood == 'MeadowV' else 0,
-        1 if new_neighborhood == 'Mitchel' else 0,
-        1 if new_neighborhood == 'NAmes' else 0,
-        1 if new_neighborhood == 'NPkVill' else 0,
-        1 if new_neighborhood == 'NWAmes' else 0,
-        1 if new_neighborhood == 'NoRidge' else 0,
-        1 if new_neighborhood == 'NridgHt' else 0,
-        1 if new_neighborhood == 'OldTown' else 0,
-        1 if new_neighborhood == 'SWISU' else 0,
-        1 if new_neighborhood == 'Sawyer' else 0,
-        1 if new_neighborhood == 'SawyerW' else 0,
-        1 if new_neighborhood == 'Somerst' else 0,
-        1 if new_neighborhood == 'StoneBr' else 0,
-        1 if new_neighborhood == 'Timber' else 0,
-        1 if new_neighborhood == 'Veenker' else 0
+        1 if new_neighborhood == i else 0 for i in nei
     ]
 
     features = list(house_dict.values()) + neighborhood1_0
